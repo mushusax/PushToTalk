9f1f79c0e28252c2f48115b7e716be5f7615d54a
mushusax <mushu.aradhyula@gmail.com>
2023-04-09 20:49:29 -0500

    ktor client websocket connection

--- app/src/main/java/com/mushusax/ptt/MainActivity.kt
+++ app/src/main/java/com/mushusax/ptt/MainActivity.kt
@@ -13,8 +13,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import io.ktor.client.*
+import io.ktor.client.engine.okhttp.*
+import io.ktor.client.plugins.websocket.*
+import io.ktor.http.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+import java.time.Duration
 
 class MainActivity : ComponentActivity() {
+
+    private val client = HttpClient(OkHttp) {
+        install(WebSockets) {
+            pingInterval = 500
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,12 +41,41 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun PTTButton() {
         Column(
-            modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Button(onClick = {})
-            { Text("BUTTON") }
+            Button(onClick = { sendHello() })
+            { Text("PTT") }
+            Button(onClick = { close() })
+            { Text("close") }
         }
+    }
+
+    private fun sendHello() {
+        runBlocking {
+            client.ws(
+                method = HttpMethod.Get,
+                host = "10.0.0.46",
+                port = 8080,
+                path = "/chat"
+            ) {
+                while(true) {
+                    val othersMessage = incoming.receive() as? Frame.Text ?: continue
+                    println(othersMessage.readText())
+                    val myMessage = "Hello"
+                    if(myMessage != null) {
+                        send(myMessage)
+                    }
+                }
+            }
+        }
+        client.close()
+    }
+
+    private fun close() {
+        client.close()
     }
 }
